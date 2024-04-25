214df85d86b91fc51362b0d9bcd8b3cd6bcafe58
Diego Rios Vasquez <diegovasquezcolombia@gmail.com>
2024-04-24 19:20:45 -0500

    Update app component and navbar component  and Create Service

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { CommentListComponent } from "./components/comment-list/comment-list.component";
+import { NavbarComponent } from "./components/navbar/navbar.component";
 
 @Component({
     selector: 'app-root',
     standalone: true,
     templateUrl: './app.component.html',
     styleUrl: './app.component.css',
-    imports: [RouterOutlet, CommentListComponent]
+    imports: [RouterOutlet, CommentListComponent, NavbarComponent]
 })
 export class AppComponent {
   title = 'Comentarios';
--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -8,5 +8,6 @@ import { Component } from '@angular/core';
   styleUrl: './navbar.component.css'
 })
 export class NavbarComponent {
+  img = 'https://www.google.com/images/branding/googlelogo/1x/googlelogo_color_272x92dp.png';
 
 }
